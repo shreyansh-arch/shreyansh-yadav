00d44ba20a6cd6138463e949dab5ed994f1d29b5
shreyansh-arch <shrey.sac15@gmail.com>
2025-06-10 10:22:41 +0530

    Create program.cs

---
+++ program.cs
@@ -0,0 +1,6 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using UserManagementAPI.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
