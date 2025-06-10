74afa8ec02de173769cc3bb96040c8e06f864226
shreyansh-arch <shrey.sac15@gmail.com>
2025-06-10 10:09:42 +0530

    Create userValidator.js

---
+++ validators/userValidator.js
@@ -0,0 +1,9 @@
+const Joi = require('joi');
+
+const userSchema = Joi.object({
+  name: Joi.string().min(3).required(),
+  email: Joi.string().email().required(),
+  age: Joi.number().integer().min(0),
+});
+
+module.exports = { userSchema };
