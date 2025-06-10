c3a108b44f0501c82bbf27a2e0eccecf5a6046f0
shreyansh-arch <shrey.sac15@gmail.com>
2025-06-10 10:40:07 +0530

    Update User.cs

--- Models/User.cs
+++ Models/User.cs
@@ -1,9 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace UserManagementAPI.Models
-{   
+{
     public class User
     {
         public int Id { get; set; }
+
+        [Required, MinLength(3)]
         public string FullName { get; set; }
+
+        [Required, EmailAddress]
         public string Email { get; set; }
     }
 }
